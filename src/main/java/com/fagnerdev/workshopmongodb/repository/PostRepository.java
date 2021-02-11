abea746185eaad1d67d894bf4401726651662c25
Fagner Cristino Dias <fcristino.sc48@gmail.com>
2021-02-11 10:14:18 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/fagnerdev/workshopmongodb/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.fagnerdev.workshopmongodb.repository;
+
+import com.fagnerdev.workshopmongodb.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
